43b3b0dada1db8669924074f30d5c5c9afa480b0
Maksim1803 <maks1803@yandex.ru>
2024-03-22 22:59:10 +0300

    First commit.

---
+++ src/Goblin.java
@@ -0,0 +1,5 @@
+public class Goblin extends FantasyCharacter {
+    public Goblin(String name, int agility, int health, int experience, int gold, int power) {
+        super(name, agility, health, experience, gold, power);
+    }
+}
---
+++ src/Skelet.java
@@ -0,0 +1,5 @@
+public class Skelet extends FantasyCharacter {
+    public Skelet(String name, int agility, int health, int experience, int gold, int power) {
+        super(name, agility, health, experience, gold, power);
+    }
+}
